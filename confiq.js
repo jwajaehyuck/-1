d6dd04cf89e7d35fd300536dba9dbf8d0abba677
whkwogur <whkwogur@naver.com>
2023-11-07 12:13:24 +0900

    todolist 구현완료

--- confiq.js
+++ confiq.js
@@ -1,19 +1,53 @@
-const textInput = document.querySelector('.text-input');
-const img1 = document.querySelector('.img1');
-const img2 = document.querySelector('.img2');
-
-const ckbox = document.querySelector('.ck-input');
 const addToDo = document.getElementById('addtodo');
-const toDo = document.getElementById('todo');
+const toDoList = document.querySelector('.todolist');
+//main 이벤트 추가
 
-const main = document.querySelector('.main');
-
-textInput.addEventListener('keypress', pressEnter);
-
-img1.addEventListener('click', clickImg1);
-
-ckbox.addEventListener('click', clickCkBox);
-
-addToDo.addEventListener('click', addtodolist);
-
-img2.addEventListener('click', removeList);
+//새로운 todo 추가하기
+addToDo.addEventListener('click', () => {
+  const adddiv1 = document.createElement('div');
+  adddiv1.className = 'main';
+  const adddiv2 = document.createElement('div');
+  const adddiv3 = document.createElement('div');
+  const addtextinput = document.createElement('input');
+  addtextinput.className = 'text-input';
+  const addckinput = document.createElement('input');
+  addckinput.className = 'ck-input';
+  addckinput.type = 'checkbox';
+  const addimg1 = document.createElement('img');
+  addimg1.src = 'img/img1.png';
+  const addimg2 = document.createElement('img');
+  addimg2.src = 'img/img2.png';
+  adddiv2.appendChild(addckinput);
+  adddiv2.appendChild(addtextinput);
+  adddiv3.appendChild(addimg1);
+  adddiv3.appendChild(addimg2);
+  adddiv1.appendChild(adddiv2);
+  adddiv1.appendChild(adddiv3);
+  //체크박스  through line 이벤트 추가
+  adddiv1.addEventListener('click', (e) => {
+    if (e.target === addckinput && e.target.checked === true) {
+      e.target.nextElementSibling.style.textDecoration = 'line-through';
+    }
+    if (e.target === addckinput && e.target.checked === false) {
+      e.target.nextElementSibling.style.textDecoration = 'none';
+    }
+  });
+  //엔터 누를시 읽기 모드 변경
+  addtextinput.addEventListener('keypress', (e) => {
+    if ((e.keyCode = 13)) {
+      if (e.target.value) {
+        e.target.readOnly = true;
+      } else alert('좋은말할때 뭐라도 입력해라');
+    }
+  });
+  //수정이미지 누를시 읽기모드 해제
+  addimg1.addEventListener('click', (e) => {
+    e.target.parentNode.previousSibling.lastChild.readOnly = false;
+  });
+  //삭제 이미지 누를시 메모 삭제
+  addimg2.addEventListener('click', (e) => {
+    e.target.parentNode.parentNode.remove();
+  });
+  toDoList.appendChild(adddiv1);
+});
+const local = {};
--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-//엔터 누르면 읽기모드
-function pressEnter(e) {
-  if (e.keyCode === 13) {
-    console.log(e.target);
-    e.target.readOnly = true;
-  }
-}
-//수정 이미지 클릭 시 읽기 모드 해제
-function clickImg1(e) {
-  console.log(e.target);
-  textInput.readOnly = false;
-}
-//체크 박스 누를 시 줄긋기
-function clickCkBox(e) {
-  console.log(e.target);
-  if (e.target.checked) {
-    textInput.style.textDecoration = 'line-through';
-  } else {
-    textInput.style.textDecoration = 'none';
-  }
-}
-//삭제 이미지 누를 시 메모내용 삭제
-function removeList() {
-  console.log(e.target);
-  main.remove();
-}
-//새로운 todo추가하기 누를 시 리스트 증가
-function addtodolist() {
-  const addsection = document.createElement('section');
-  addsection.className = 'main';
-  const addckinput = document.createElement('input');
-  addckinput.className = 'ck-input';
-  addckinput.type = 'checkbox';
-  const addtextinput = document.createElement('input');
-  addtextinput.className = 'text-input';
-  const addimg1 = document.createElement('img');
-  addimg1.className = 'img1';
-  addimg1.src = 'img/img1.png';
-  const addimg2 = document.createElement('img');
-  addimg2.className = 'img2';
-  addimg2.src = 'img/img2.png';
-
-  addsection.append(addckinput, addtextinput, addimg1, addimg2);
-  todo.append(addsection);
-}
+////엔터 누르면 읽기모드
+//function pressEnter(e) {
+//  if (e.keyCode === 13) {
+//    e.target.readOnly = true;
+//  }
+//}
+////수정 이미지 클릭 시 읽기 모드 해제
+//function clickImg1(e) {
+//  textInput.readOnly = false;
+//}
+////체크 박스 누를 시 줄긋기
+//function clickCkBox(e) {
+//  if (e.target.checked) {
+//    console.log(e.target.nextElementSibling);
+//    textInput.style.textDecoration = 'line-through';
+//  } else {
+//    textInput.style.textDecoration = 'none';
+//  }
+//}
+////삭제 이미지 누를 시 메모내용 삭제
+//function removeList() {
+//  main.remove();
+//}
+//
+//function addtodolist() {
+//  const addsection = document.csreateElement('section');
+//  addsection.className = 'main';
+//  const addckinput = document.createElement('input');
+//  addckinput.className = 'ck-input';
+//  addckinput.type = 'checkbox';
+//  const addtextinput = document.createElement('input');
+//  addtextinput.className = 'text-input';
+//  const addimg1 = document.createElement('img');
+//  addimg1.className = 'img1';
+//  addimg1.src = 'img/img1.png';
+//  const addimg2 = document.createElement('img');
+//  addimg2.className = 'img2';
+//  addimg2.src = 'img/img2.png';
+//  addsection.append(addckinput, addtextinput, addimg1, //addimg2);
+//  todo.append(addsection);
+//}
